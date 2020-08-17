f5bd7867aa96402bf059edb0be64f6936d6eb1db
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-08-16 23:24:52 -0300

    Updates

---
+++ precache-manifest.0d3992dad8f82c3ca95adc677bf01803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fe12a2bacfc3a225d408f523cd9732",
+    "url": "/bitcoin-explorer/index.html"
+  },
+  {
+    "revision": "69e8cd783da19807dd36",
+    "url": "/bitcoin-explorer/static/js/2.f2a2dce6.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/bitcoin-explorer/static/js/2.f2a2dce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba97f4f218250cd0621e",
+    "url": "/bitcoin-explorer/static/js/main.80be5356.chunk.js"
+  },
+  {
+    "revision": "da79ad480e4f5037c6c2",
+    "url": "/bitcoin-explorer/static/js/runtime-main.2ac731d7.js"
+  }
+]);
